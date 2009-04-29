79a0cb53a0c7ab6dac29ad24f0b96d756d874171
stephenfenech <stephenfenech@f428e11c-c467-0410-921a-bf1abd275347>
2009-04-29 15:04:29 +0000

    Forgot to save the changes...

--- examples-dist/versiontranslationproxy/banzai.bat
+++ examples-dist/versiontranslationproxy/banzai.bat
@@ -8,7 +8,7 @@ cd %~dp0
 set CP=%MULE_HOME%/lib/user/mina-core-1.1.0.jar;../lib/slf4j-api-1.3.0.jar;../lib/slf4j-jdk14-1.3.0.jar;%MULE_HOME%\lib\user\quickfixj-all-1.3.3.jar;../lib/quickfixj-examples-1.3.3.jar
 
 
-java -cp %CP% quickfix.examples.banzai.Banzai %~dp0../../../examples/src/main/resources/versiontranslationproxy/banzai.cfg 
+java -cp %CP% quickfix.examples.banzai.Banzai %~dp0../../examples/src/main/resources/versiontranslationproxy/banzai.cfg 
 
 pause
 
